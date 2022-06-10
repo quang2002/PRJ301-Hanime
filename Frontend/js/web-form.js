f074948ca755ab4f1d469ac027c0608e3325af35
quang2002 <yuyu2711@outlook.com>
2022-06-10 16:47:10 +0700

    remake frontend

--- Frontend/js/web-form.js
+++
@@ -1,7 +0,0 @@
-function webFormInputNotEmpty(i) {
-    if (i.value.length > 0) {
-        i.classList.add('web__form__input__not__empty');
-    } else {
-        i.classList.remove('web__form__input__not__empty');
-    }
-}
